32c678ef016f254c85acee475508009ae9bdac39
aswin288 <aswin.ravi288@gmail.com>
2024-05-19 14:08:59 +0530

    simple interest

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+
+import './App.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+function App() {
+  //state to hold value
+  const [principle, setPrinciple] = useState(0)
+  const [rate, setRate] = useState(0)
+  const [year, setYear] = useState(0)
+  const [interest,setinterest] = useState(0)
+
+  //conditionally render
+  const [isPrinciple, setIsPrinciple] = useState(true)
+  const [isRate, setIsRate] = useState(true)
+  const [isYear, setIsYear] = useState(true)
+
+
+
+
+
+
+
+
+
+  const validate = (e) => {
+    const { name, value } = e.target
+    console.log(name);
+    console.log(value);
+    //regular
+
+    if (!!value.match(/^[0-9]*$/)) {
+      if (name == 'principle') {
+        setPrinciple(value)
+        setIsPrinciple(true)
+      }
+      else if (name == 'rate') {
+        setRate(value)
+        setIsRate(true)
+      }
+      else {
+        setYear(value)
+        setIsYear(true)
+      }
+    } else {
+      if (name == 'principle') {
+        setPrinciple(value)
+        setIsPrinciple(false)
+      }
+      else if (name == 'rate') {
+        setRate(value)
+        setIsRate(false)
+      }
+      else {
+        setYear(value)
+        setIsYear(false)
+      }
+
+    }
+  }
+
+
+// function to reset
+  const handleReset=()=>{
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+    setIsPrinciple(true)
+    setIsRate(true)
+    setIsYear(true)
+    setinterest(0)
+  }
+
+  //function to calculate simple interest
+  const handleCalculate=()=>{
+    setinterest((principle*rate*year)/100)
+  }
+
+  return (
+    <>
+      <div className='main'>
+        <div className='sub p-5'>
+          <h1>simple interest app</h1>
+          <p>Calculate your simple interest easily</p>
+
+          <div className='w-100 d-flex justify-content-center align-items-center result rounded mt-5 shadow flex-column'>
+            <h1>₹ {interest}</h1>
+            <p>Total simple interest</p>
+
+          </div>
+
+
+
+          <form action="" className='mt-5'>
+            <TextField id="outlined-basic" name='principle' value={principle ||""} label="₹ Principle Amount" variant="outlined" className='w-100' onChange={(e) => validate(e)} />
+           {!isPrinciple && <p className='text-danger'>Invalid input</p>}
+            <TextField id="outlined-basic"
+              name='rate' value={rate ||""} label="Rate of interest (p.a) %" variant="outlined" className='w-100 mt-3' onChange={(e) => validate(e)} />
+           {!isRate &&<p className='text-danger'>Invalid input</p>}
+            <TextField id="outlined-basic" name='year' value={year ||""} label="Year(Yr)" variant="outlined" className='w-100 mt-3' onChange={(e) => validate(e)} />
+           {!isYear && <p className='text-danger'>Invalid input</p>}
+
+            <div className='d-flex mt-4'>
+              <Button onClick={handleCalculate} className='w-50 p-3 me-3' variant="contained" color='success' disabled={isPrinciple && isRate && isYear ?false:true}>CALCULATE</Button>
+              <Button onClick={handleReset} className='w-50 p-3' variant="outlined" color='primary'>RESET</Button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+/*SI = PRY/100 */ 
